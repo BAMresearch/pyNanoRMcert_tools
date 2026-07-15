2b84a93f162be575eee9afba02e9441039667c06
Ingo Breßler <ingo.bressler@bam.de>
2026-07-15 19:05:32 +0200

    fix(matplotlib): update predefined colors

--- my_definitions_matplotlib.py
+++ my_definitions_matplotlib.py
@@ -16,32 +16,32 @@ class BAMColors:
     """Definition of BAM colors according to the corporate design of BAM"""
 
     # --- color accents of black ---
-    black    = '#070d0d' # primary color 
-    black_80 = '#273d3d' 
-    black_60 = '#506e6e' 
-    black_45 = '#749191'
-    black_30 = '#9eb8b8'
-    black_15 = '#ccdbdb'
+    black    = '#002832' # primary color
+    black_80 = '#325463'
+    black_60 = '#5F7B89'
+    black_45 = '#7D95A3'
+    black_30 = '#7D95A3'
+    black_15 = '#CCD8DF'
     # --- The four color accents of red ---
-    red      = '#E60000' # primary color
-    red_2    = '#b80000'
-    red_3    = '#7c0000'
-    red_4    = '#330000'
+    red      = '#D2001E' # primary color
+    red_2    = '#B40F1E'
+    red_3    = '#8C1419'
+    red_4    = '#501919'
     # --- The four color accents of blue ---
-    blue     = '#00ffff' # primary color
-    blue_2   = '#13bfbf'
-    blue_3   = '#1c8c8c'
-    blue_4   = '#174d4d'
+    blue     = '#00AFF0' # primary color
+    blue_2   = '#0089BA'
+    blue_3   = '#007599'
+    blue_4   = '#00556E'
     #--- The four color accents of green ---
-    green    = '#73e600'
-    green_2  = '#66cc14'
-    green_3  = '#47b312'
-    green_4  = '#2e990f'
+    green    = '#8CB40F'
+    green_2  = '#82A532'
+    green_3  = '#5A8C32'
+    green_4  = '#377832'
     #--- The four color accents of yellow ---
-    yellow   = '#ffe600'
-    yellow_2 = '#ffb300'
-    yellow_3 = '#fe68a0'
-    yellow_4 = '#cc6600' 
+    yellow   = '#FFDC00'
+    yellow_2 = '#FAB900'
+    yellow_3 = '#E69B00'
+    yellow_4 = '#CD7D00'
 
 class BAMMarkerStyles:
     """Style of markers in plots with BAM colors"""
@@ -52,7 +52,7 @@ class BAMMarkerStyles:
                     markersize=5, markeredgewidth=1, markeredgecolor=BAMColors.red)
     green = dict(color=BAMColors.green, linestyle='', marker='x', fillstyle='none',
                     markersize=5, markeredgewidth=1, markeredgecolor=BAMColors.green)
-    
+
     blue = dict(color=BAMColors.blue, linestyle='', marker='^', fillstyle='none',
                     markersize=5, markeredgewidth=1, markeredgecolor=BAMColors.blue)
     yellow = dict(color=BAMColors.yellow, linestyle='', marker='^', fillstyle='none',
